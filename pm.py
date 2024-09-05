4b3bcc46a8108107105fa884a070d1355e3cd255
towelie <mail@towelie.dev>
2024-09-05 11:32:05 +0200

    changed visual feedback and j,k movement

--- pm.py
+++ pm.py
@@ -8,26 +8,62 @@ def run_command(command):
     return subprocess.run(command, capture_output=True, text=True, shell=True).stdout.strip()
 
 def get_repos():
-    repos = run_command("gh repo list --json name --jq '.[].name'").split('\n')
-    return [repo for repo in repos if repo]
+    all_repos = run_command("gh repo list --json name --jq '.[].name'").split('\n')
+    existing_repos = os.listdir(os.path.expanduser("~/projects"))
+    return [repo for repo in all_repos if repo and repo not in existing_repos]
+
+class CustomCheckBox(urwid.WidgetWrap):
+    def __init__(self, label):
+        self.checkbox = urwid.CheckBox(label)
+        self.attr = urwid.AttrMap(self.checkbox, 'normal', 'highlight')
+        super().__init__(self.attr)
+
+    @property
+    def state(self):
+        return self.checkbox.state
+
+    def toggle_state(self):
+        self.checkbox.toggle_state()
+
+    def set_state(self, state):
+        self.checkbox.set_state(state)
+
+    @property
+    def label(self):
+        return self.checkbox.label
 
 class RepoSelector(urwid.ListBox):
     def __init__(self, repos):
         self.repos = repos
-        self.selected = set()
-        body = [urwid.CheckBox(repo) for repo in repos]
+        body = [CustomCheckBox(repo) for repo in repos]
         super().__init__(urwid.SimpleFocusListWalker(body))
 
     def keypress(self, size, key):
-        if key == 'enter':
+        if key in ('j', 'k'):
+            return super().keypress(size, 'down' if key == 'j' else 'up')
+        elif key == 'G':
+            self.set_focus(len(self.body) - 1)
+            self._invalidate()
+        elif key == 'g':
+            if self._command_map[key] == 'cursor max':
+                self.set_focus(0)
+                self._invalidate()
+            else:
+                return key
+        elif key == 'enter':
             self.clone_selected()
             raise urwid.ExitMainLoop()
+        elif key == ' ':
+            checkbox = self.focus
+            if isinstance(checkbox, CustomCheckBox):
+                checkbox.toggle_state()
+            return None
         return super().keypress(size, key)
 
     def clone_selected(self):
         for checkbox in self.body:
-            if checkbox.get_state():
-                repo = checkbox.get_label()
+            if checkbox.state:
+                repo = checkbox.label
                 print(f"Cloning {repo}...")
                 run_command(f"gh repo clone {repo} ~/projects/{repo}")
         print("Cloning completed.")
@@ -35,7 +71,13 @@ class RepoSelector(urwid.ListBox):
 def clone_repos():
     repos = get_repos()
     selector = RepoSelector(repos)
-    urwid.MainLoop(selector).run()
+    palette = [
+        ('normal', 'light gray', 'black'),
+        ('highlight', 'black', 'light gray'),
+        ('selected', 'black', 'dark cyan'),
+    ]
+    loop = urwid.MainLoop(selector, palette)
+    loop.run()
 
 def get_repo_status(repo_path):
     repo = git.Repo(repo_path)
